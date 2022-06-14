e5535d2a01e964d41742681f852800abc66aee9f
slavisam <slavisam@gmail.com>
2022-06-14 17:28:38 +0200

    ref: use map instead of static var

---
+++ design_system/colors.dart
@@ -0,0 +1,449 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  /// primary : on-primary
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ❌ Fail
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysLightPrimary = Color(0xff6750a4); 
+  
+  static const m3SysLightOnPrimary = Color(0xffffffff); 
+  
+  /// primary-container : on-primary-container
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysLightPrimaryContainer = Color(0xffeaddff); 
+  
+  static const m3SysLightOnPrimaryContainer = Color(0xff21005d); 
+  
+  /// secondary : on-secondary
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ❌ Fail
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysLightSecondary = Color(0xff625b71); 
+  
+  static const m3SysLightOnSecondary = Color(0xffffffff); 
+  
+  /// secondary-container : on-secondary-container
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysLightSecondaryContainer = Color(0xffe8def8); 
+  
+  static const m3SysLightOnSecondaryContainer = Color(0xff1d192b); 
+  
+  /// tertiary-container : on-tertiary-container
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysLightTertiaryContainer = Color(0xffffd8e4); 
+  
+  static const m3SysLightOnTertiaryContainer = Color(0xff31111d); 
+  
+  /// error : on-error
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ❌ Fail
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysLightError = Color(0xffb3261e); 
+  
+  /// surface : on-surface
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysLightSurface = Color(0xfffffbfe); 
+  
+  static const m3SysLightOnSurface = Color(0xff1c1b1f); 
+  
+  /// surface-variant : on-surface-variant
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysLightSurfaceVariant = Color(0xffe7e0ec); 
+  
+  static const m3SysLightOnSurfaceVariant = Color(0xff49454f); 
+  
+  static const m3SysLightOutline = Color(0xff79747e); 
+  
+  static const m3SysLightInverseOnSurface = Color(0xfff4eff4); 
+  
+  static const m3SysLightInverseSurface = Color(0xff313033); 
+  
+  /// primary : on-primary
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysDarkPrimary = Color(0xffd0bcff); 
+  
+  static const m3SysDarkOnPrimary = Color(0xff381e72); 
+  
+  /// primary-container : on-primary-container
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysDarkPrimaryContainer = Color(0xff4f378b); 
+  
+  static const m3SysDarkOnPrimaryContainer = Color(0xffeaddff); 
+  
+  static const m3SysDarkOnSecondary = Color(0xff332d41); 
+  
+  /// secondary-container : on-secondary-container
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysDarkSecondaryContainer = Color(0xff4a4458); 
+  
+  static const m3SysDarkOnSecondaryContainer = Color(0xffe8def8); 
+  
+  static const m3SysDarkOnTertiary = Color(0xff492532); 
+  
+  /// tertiary-container : on-tertiary-container
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysDarkTertiaryContainer = Color(0xff633b48); 
+  
+  static const m3SysDarkOnTertiaryContainer = Color(0xffffd8e4); 
+  
+  /// error : on-error
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysDarkError = Color(0xfff2b8b5); 
+  
+  static const m3SysDarkOnError = Color(0xff601410); 
+  
+  /// surface : on-surface
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysDarkSurface = Color(0xff1c1b1f); 
+  
+  static const m3SysDarkOnSurface = Color(0xffe6e1e5); 
+  
+  /// surface-variant : on-surface-variant
+  /// 
+  /// Regular text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ❌ Fail
+  /// 
+  /// Large text:
+  /// Level AA - ✅ Pass
+  /// Level AAA - ✅ Pass
+  /// 
+  /// Icons:
+  /// Level AA - ✅ Pass
+  static const m3SysDarkSurfaceVariant = Color(0xff49454f); 
+  
+  static const m3SysDarkOnSurfaceVariant = Color(0xffcac4d0); 
+  
+  static const m3SysDarkOutline = Color(0xff938f99); 
+  
+  static const m3SysDarkInverseOnSurface = Color(0xff1c1b1f); 
+  
+  static const m3SysDarkInverseSurface = Color(0xffe6e1e5); 
+  
+  static const m3RefPrimaryPrimary99 = Color(0xfffffbfe); 
+  
+  static const m3RefPrimaryPrimary95 = Color(0xfff6edff); 
+  
+  static const m3RefPrimaryPrimary80 = Color(0xffd0bcff); 
+  
+  static const m3RefPrimaryPrimary60 = Color(0xff9a82db); 
+  
+  static const m3RefPrimaryPrimary50 = Color(0xff7f67be); 
+  
+  static const m3RefPrimaryPrimary30 = Color(0xff4f378b); 
+  
+  static const m3RefPrimaryPrimary20 = Color(0xff381e72); 
+  
+  static const m3RefPrimaryPrimary10 = Color(0xff21005d); 
+  
+  static const m3RefSecondarySecondary90 = Color(0xffe8def8); 
+  
+  static const m3RefSecondarySecondary80 = Color(0xffccc2dc); 
+  
+  static const m3RefSecondarySecondary70 = Color(0xffb0a7c0); 
+  
+  static const m3RefSecondarySecondary60 = Color(0xff958da5); 
+  
+  static const m3RefSecondarySecondary50 = Color(0xff7a7289); 
+  
+  static const m3RefSecondarySecondary40 = Color(0xff625b71); 
+  
+  static const m3RefSecondarySecondary30 = Color(0xff4a4458); 
+  
+  static const m3RefSecondarySecondary20 = Color(0xff332d41); 
+  
+  static const m3RefSecondarySecondary10 = Color(0xff1d192b); 
+  
+  static const m3RefTertiaryTertiary99 = Color(0xfffffbfa); 
+  
+  static const m3RefTertiaryTertiary95 = Color(0xffffecf1); 
+  
+  static const m3RefTertiaryTertiary90 = Color(0xffffd8e4); 
+  
+  static const m3RefTertiaryTertiary80 = Color(0xffefb8c8); 
+  
+  static const m3RefTertiaryTertiary70 = Color(0xffd29dac); 
+  
+  static const m3RefTertiaryTertiary60 = Color(0xffb58392); 
+  
+  static const m3RefTertiaryTertiary50 = Color(0xff986977); 
+  
+  static const m3RefTertiaryTertiary40 = Color(0xff7d5260); 
+  
+  static const m3RefTertiaryTertiary30 = Color(0xff633b48); 
+  
+  static const m3RefTertiaryTertiary20 = Color(0xff492532); 
+  
+  static const m3RefTertiaryTertiary10 = Color(0xff31111d); 
+  
+  static const m3RefNeutralNeutral95 = Color(0xfff4eff4); 
+  
+  static const m3RefNeutralNeutral90 = Color(0xffe6e1e5); 
+  
+  static const m3RefNeutralNeutral80 = Color(0xffc9c5ca); 
+  
+  static const m3RefNeutralNeutral70 = Color(0xffaeaaae); 
+  
+  static const m3RefNeutralNeutral60 = Color(0xff939094); 
+  
+  static const m3RefNeutralNeutral50 = Color(0xff787579); 
+  
+  static const m3RefNeutralNeutral40 = Color(0xff605d62); 
+  
+  static const m3RefNeutralNeutral30 = Color(0xff484649); 
+  
+  static const m3RefNeutralNeutral20 = Color(0xff313033); 
+  
+  static const m3RefNeutralVariantNeutralVariant95 = Color(0xfff5eefa); 
+  
+  static const m3RefNeutralVariantNeutralVariant90 = Color(0xffe7e0ec); 
+  
+  static const m3RefNeutralVariantNeutralVariant80 = Color(0xffcac4d0); 
+  
+  static const m3RefNeutralVariantNeutralVariant70 = Color(0xffaea9b4); 
+  
+  static const m3RefNeutralVariantNeutralVariant60 = Color(0xff938f99); 
+  
+  static const m3RefNeutralVariantNeutralVariant50 = Color(0xff79747e); 
+  
+  static const m3RefNeutralVariantNeutralVariant40 = Color(0xff605d66); 
+  
+  static const m3RefNeutralVariantNeutralVariant30 = Color(0xff49454f); 
+  
+  static const m3RefNeutralVariantNeutralVariant20 = Color(0xff322f37); 
+  
+  static const m3RefNeutralVariantNeutralVariant10 = Color(0xff1d1a22); 
+  
+  static const m3RefErrorError99 = Color(0xfffffbf9); 
+  
+  static const m3RefErrorError95 = Color(0xfffceeee); 
+  
+  static const m3RefErrorError90 = Color(0xfff9dedc); 
+  
+  static const m3RefErrorError80 = Color(0xfff2b8b5); 
+  
+  static const m3RefErrorError70 = Color(0xffec928e); 
+  
+  static const m3RefErrorError60 = Color(0xffe46962); 
+  
+  static const m3RefErrorError50 = Color(0xffdc362e); 
+  
+  static const m3RefErrorError30 = Color(0xff8c1d18); 
+  
+  static const m3RefErrorError20 = Color(0xff601410); 
+  
+  static const m3RefErrorError10 = Color(0xff410e0b); 
+  
+  static const m3ReadOnlyLightSurface1 = Color(0xfff7f2fa); 
+  
+  static const m3ReadOnlyLightSurface3 = Color(0xffeee8f4); 
+  
+  static const m3ReadOnlyLightSurface4 = Color(0xffede6f3); 
+  
+  static const m3ReadOnlyLightSurface5 = Color(0xffeae3f1); 
+  
+  static const m3ReadOnlyLightBlack = Color(0xff000000); 
+  
+  static const m3ReadOnlyLightWhite = Color(0xffffffff); 
+  
+  static const m3ReadOnlyLightPrimaryOpacity008 = Color(0x146750a4); 
+  
+  static const m3ReadOnlyLightPrimaryOpacity012 = Color(0x1f6750a4); 
+  
+  static const m3ReadOnlyLightOnPrimaryOpacity008 = Color(0x14ffffff); 
+  
+  static const m3ReadOnlyLightOnPrimaryOpacity012 = Color(0x1fffffff); 
+  
+  static const m3ReadOnlyLightOnPrimaryContainerOpacity008 = Color(0x1421005d); 
+  
+  static const m3ReadOnlyLightOnPrimaryContainerOpacity012 = Color(0x1f21005d); 
+  
+  static const m3ReadOnlyLightOnSecondaryContainerOpacity008 = Color(0x141d192b); 
+  
+  static const m3ReadOnlyLightOnSecondaryContainerOpacity012 = Color(0x1f1d192b); 
+  
+  static const m3ReadOnlyLightOnSecondaryContainerOpacity016 = Color(0x291d192b); 
+  
+  static const m3ReadOnlyLightOnTertiaryContainerOpacity008 = Color(0x1431111d); 
+  
+  static const m3ReadOnlyLightOnTertiaryContainerOpacity012 = Color(0x1f31111d); 
+  
+  static const m3ReadOnlyLightOnSurfaceVariantOpacity008 = Color(0x1449454f); 
+  
+  static const m3ReadOnlyLightOnSurfaceVariantOpacity012 = Color(0x1f49454f); 
+  
+  static const m3ReadOnlyLightOnSurfaceVariantOpacity016 = Color(0x2949454f); 
+  
+  static const m3ReadOnlyLightOnSurfaceOpacity012 = Color(0x1f1c1b1f); 
+  
+  static const m3ReadOnlyLightOnSurfaceOpacity016 = Color(0x291c1b1f); 
+  
+  static const m3ReadOnlyDarkSurface1 = Color(0xff25232a); 
+  
+  static const m3ReadOnlyDarkSurface2 = Color(0xff2a2831); 
+  
+  static const m3ReadOnlyDarkSurface3 = Color(0xff302d38); 
+  
+  static const m3ReadOnlyDarkSurface4 = Color(0xff322e3a); 
+  
+  static const m3ReadOnlyDarkSurface5 = Color(0xff35323e); 
+  
+  static const m3ReadOnlyDarkPrimaryOpacity008 = Color(0x14d0bcff); 
+  
+  static const m3ReadOnlyDarkPrimaryOpacity012 = Color(0x1fd0bcff); 
+  
+  static const m3ReadOnlyDarkOnPrimaryContainerOpacity008 = Color(0x14eaddff); 
+  
+  static const m3ReadOnlyDarkOnPrimaryContainerOpacity012 = Color(0x1feaddff); 
+  
+  static const m3ReadOnlyDarkOnSecondaryContainerOpacity008 = Color(0x14e8def8); 
+  
+  static const m3ReadOnlyDarkOnSecondaryContainerOpacity012 = Color(0x1fe8def8); 
+  
+  static const m3ReadOnlyDarkOnSecondaryContainerOpacity016 = Color(0x29e8def8); 
+  
+  static const m3ReadOnlyDarkOnTertiaryContainerOpacity008 = Color(0x14ffd8e4); 
+  
+  static const m3ReadOnlyDarkOnTertiaryContainerOpacity012 = Color(0x1fffd8e4); 
+  
+  static const m3ReadOnlyDarkOnSurfaceOpacity012 = Color(0x1fe6e1e5); 
+  
+  static const m3ReadOnlyDarkSurfaceVariantOpacity012 = Color(0x1f49454f); 
+  
+  AppColors._();
+}
---
+++ design_system/text_styles.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const m3Headline6 = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 18,
+    decoration: TextDecoration.none,
+    letterSpacing: 0,
+  );
+
+  static const m3DisplayLarge = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 57,
+    decoration: TextDecoration.none,
+    letterSpacing: -0.25079999999999997,
+  );
+
+  static const m3DisplayMedium = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 45,
+    decoration: TextDecoration.none,
+    letterSpacing: 0,
+  );
+
+  static const m3DisplaySmall = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 36,
+    decoration: TextDecoration.none,
+    letterSpacing: 0,
+  );
+
+  static const m3HeadlineLarge = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 32,
+    decoration: TextDecoration.none,
+    letterSpacing: 0,
+  );
+
+  static const m3HeadlineMedium = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 28,
+    decoration: TextDecoration.none,
+    letterSpacing: 0,
+  );
+
+  static const m3HeadlineSmall = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 24,
+    decoration: TextDecoration.none,
+    letterSpacing: 0,
+  );
+
+  static const m3TitleLarge = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 22,
+    decoration: TextDecoration.none,
+    letterSpacing: 0,
+  );
+
+  static const m3TitleMedium = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.normal,
+    fontSize: 16,
+    decoration: TextDecoration.none,
+    letterSpacing: 0.1008,
+  );
+
+  static const m3TitleSmall = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.normal,
+    fontSize: 14,
+    decoration: TextDecoration.none,
+    letterSpacing: 0.0994,
+  );
+
+  static const m3LabelLarge = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.normal,
+    fontSize: 14,
+    decoration: TextDecoration.none,
+    letterSpacing: 0.0994,
+  );
+
+  static const m3LabelMedium = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.normal,
+    fontSize: 12,
+    decoration: TextDecoration.none,
+    letterSpacing: 0.5004,
+  );
+
+  static const m3LabelSmall = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.normal,
+    fontSize: 11,
+    decoration: TextDecoration.none,
+    letterSpacing: 0.5005,
+  );
+
+  static const m3BodyLarge = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 16,
+    decoration: TextDecoration.none,
+    letterSpacing: 0.5008,
+  );
+
+  static const m3BodyMedium = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 14,
+    decoration: TextDecoration.none,
+    letterSpacing: 0.25060000000000004,
+  );
+
+  static const m3BodySmall = TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontSize: 12,
+    decoration: TextDecoration.none,
+    letterSpacing: 0.3996,
+  );
+
+  AppTextStyles._();
+}
